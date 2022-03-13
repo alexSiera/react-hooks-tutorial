013318f59cbac90de74a2e4d6b7fdf9cdf0609dc
devIntCool <dev@interactivecoolture.com>
2022-03-13 14:34:07 +0300

    Initialize project

--- src/components/IsFive.jsx
+++ src/components/IsFive.jsx
@@ -1,24 +1,15 @@
-import React from 'react';
+import React from "react";
 
 let renderCount = 0;
 
-export default React.memo(
-  function IsFive({ value }) {
-    console.warn(`🔴 isFive render: ${++renderCount}`);
+export default function IsFive({ value }) {
+  console.warn(`🔴 isFive render: ${++renderCount}`);
 
-    const getResult = React.useMemo(() => {
-      let i = 0;
-      while (i < 600000000) i++;
-      return value === 5 ? '✅ Это пять :D' : '❌ Это не пять :(';
-    }, [value]);
+  const getResult = () => {
+    let i = 0;
+    while (i < 600000000) i++;
+    return value === 5 ? "✅ Это пять :D" : "❌ Это не пять :(";
+  };
 
-    return <h3>{getResult}</h3>;
-  },
-  (prevProps, nextPropx) => {
-    if (nextPropx.value === 5) {
-      return false;
-    } else {
-      return true;
-    }
-  },
-);
+  return <h3>{getResult()}</h3>;
+}
