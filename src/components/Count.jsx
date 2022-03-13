59ab1f19ff04ef1cdae0754b60905eec03c529a9
devIntCool <dev@interactivecoolture.com>
2022-03-13 15:57:34 +0300

    Use memo and useMemo

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -5,11 +5,13 @@ const render = {
   count2: 0,
 };
 
-export default function Count({ id, value }) {
+// memo render component
+
+export default React.memo(function Count({ id, value }) {
   console.warn(` Count${id} render: ${++render[`count${id}`]}`);
   return (
     <div>
       <h1 className="cyan">{value}</h1>
     </div>
   );
-}
+});
